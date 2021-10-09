5a015d519c7dbef8af748392de2417f09e77c9eb
Anthony Tejada <anthony_0250hotmail.com>
2021-10-08 20:01:10 -0500

    Application CodeFirstProducts

---
+++ CodeFirstApplication/Controllers/AdminProductController.cs
@@ -0,0 +1,134 @@
+﻿using CodeFirstApplication.Data;
+using CodeFirstApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeFirstApplication.Controllers
+{
+    public class AdminProductController : Controller
+    {
+        ApplicationDbContext _dbContext = new ApplicationDbContext();
+
+        public IActionResult Products()
+        {
+            var products = _dbContext.Products;
+            List<ProductModel> productsList = products.ToList();
+            return View(productsList);
+        }
+
+        public IActionResult UpsertProduct(int? id)
+        {
+            var products = _dbContext.Products;
+            IEnumerable<SelectListItem> productsList = GetCategories(); //(IEnumerable<SelectListItem>)products.ToList();
+
+            NewProduct newProduct = new NewProduct();
+            newProduct.CategoryList = productsList;
+
+            if (id != null)
+            {
+                List<ProductModel> productList = _dbContext.Products.ToList();
+                ProductModel product = productList.Where(x => x.IdProduct == id).FirstOrDefault();
+                newProduct.Product = product;
+                return View(newProduct);
+            }
+            else {
+                newProduct.Product = new ProductModel();
+            }
+            return View(newProduct);
+        }
+
+        [HttpPost]
+        public IActionResult UpsertProduct(NewProduct productFormData)
+        {
+            //var products = _dbContext.Products;
+            //IEnumerable<SelectListItem> productsList = (IEnumerable<SelectListItem>)products.ToList();
+
+            if (ModelState.IsValid)
+            {
+                if (productFormData.Product.IdProduct != 0)
+                {
+                    ProductModel existingProduct = _dbContext.Products.FirstOrDefault(p => p.IdProduct == productFormData.Product.IdProduct);
+                    existingProduct.NameProduct = productFormData.Product.NameProduct;
+                    existingProduct.DescriptionProduct = productFormData.Product.DescriptionProduct;
+                    existingProduct.ImageUrlProduct = productFormData.Product.ImageUrlProduct;
+                    existingProduct.MemoryProduct = productFormData.Product.MemoryProduct;
+                    existingProduct.StorageCapacityProduct = productFormData.Product.StorageCapacityProduct;
+                    existingProduct.ResolutionImageProduct = productFormData.Product.ResolutionImageProduct;
+                    existingProduct.PriceProduct = productFormData.Product.PriceProduct;
+                    existingProduct.CategoryId = productFormData.Product.CategoryId;
+
+                    _dbContext.Update(existingProduct);
+                    _dbContext.SaveChanges();
+                }
+                else
+                {
+                    ProductModel newProduct = new ProductModel();
+                    newProduct = productFormData.Product;
+
+                    _dbContext.Update(newProduct);
+                    _dbContext.SaveChanges();
+                }
+
+                return RedirectToAction(nameof(Products));
+            }
+            else {
+                productFormData.CategoryList = GetCategories(); //productsList;
+                if (productFormData.Product.IdProduct != 0) {
+                    productFormData.Product = _dbContext.Products.FirstOrDefault(p => p.IdProduct == productFormData.Product.IdProduct);
+                }
+            }
+
+            return View(productFormData);
+        }
+
+
+        public IActionResult DeleteProduct(int? id)
+        {
+            var products = _dbContext.Products;
+            IEnumerable<SelectListItem> productsList = GetCategories(); //(IEnumerable<SelectListItem>)products.ToList();
+
+            NewProduct newProduct = new NewProduct();
+            newProduct.CategoryList = productsList;
+
+            if (id != null)
+            {
+                List<ProductModel> productList = _dbContext.Products.ToList();
+                ProductModel product = productList.Where(x => x.IdProduct == id).FirstOrDefault();
+                newProduct.Product = product;
+                return View(newProduct);
+            }
+            else {
+                return RedirectToAction(nameof(Products));
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult DeleteProduct(NewProduct productFormData) 
+        {
+            if (ModelState.IsValid)
+            {
+                if (productFormData.Product.IdProduct != 0)
+                {
+                    _dbContext.Remove(_dbContext.Products.Single(p => p.IdProduct == productFormData.Product.IdProduct));
+                    _dbContext.SaveChanges();
+                    return RedirectToAction(nameof(Products));
+                }
+            }
+            return RedirectToAction(nameof(Products));
+        }
+
+
+        public List<SelectListItem> GetCategories()
+        {
+            return new List<SelectListItem>()
+            {
+                new SelectListItem() {Value = "1" , Text = "Smartphone" },
+                new SelectListItem() {Value = "2" , Text = "Consoles" },
+                new SelectListItem() {Value = "3" , Text = "Laptops" }
+            };
+        }
+    }
+}
---
+++ CodeFirstApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using CodeFirstApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApplication.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<CategoryModel> Categories { set; get; }
+        public DbSet<ProductModel> Products { set; get; }
+
+        public ApplicationDbContext() {  }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseSqlServer("Data Source=(localdb)\\LAPTOP-VBJJ1N26;Initial Catalog=CodeFirstBooks;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                optionsBuilder.UseSqlServer("server=LAPTOP-VBJJ1N26;database=CodeFirstBooks;trusted_connection=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CategoryModel>().HasData(
+                new CategoryModel() { IdCategory = 1, NameCategory = "Smartphone" },
+                new CategoryModel() { IdCategory = 2, NameCategory = "Consoles" },
+                new CategoryModel() { IdCategory = 3, NameCategory = "Laptops" });
+        }
+
+    }
+}
